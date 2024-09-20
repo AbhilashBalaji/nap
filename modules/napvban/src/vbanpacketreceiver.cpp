f426b73cad382db960c434778baa3ca5879b552a
Abhilash Balaji <abhilash.balaji1998@gmail.com>
2024-09-20 16:08:08 +0200

    optimisation

--- modules/napvban/src/vbanpacketreceiver.cpp
+++ modules/napvban/src/vbanpacketreceiver.cpp
@@ -11,7 +11,7 @@
 #include "vbanutils.h"
 
 RTTI_BEGIN_CLASS(nap::VBANPacketReceiver)
-RTTI_PROPERTY("Server", &nap::VBANPacketReceiver::mServer, nap::rtti::EPropertyMetaData::Required)
+    RTTI_PROPERTY("Server", &nap::VBANPacketReceiver::mServer, nap::rtti::EPropertyMetaData::Required)
 RTTI_END_CLASS
 
 namespace nap
@@ -21,8 +21,8 @@ namespace nap
 	{
         mServer->registerListenerSlot(mPacketReceivedSlot);
 
-		return true;
-	}
+        return true;
+    }
 
 
 	void VBANPacketReceiver::packetReceived(const UDPPacket &packet)
@@ -30,51 +30,49 @@ namespace nap
         // Process adding or removing receivers
         mTaskQueue.process();
 
-		utility::ErrorState errorState;
+        utility::ErrorState errorState;
 		if (checkPacket(errorState, &packet.data()[0], packet.size()) ) {
             struct VBanHeader const *const hdr = (struct VBanHeader *) (&packet.data()[0]);
 
-            // get packet meta-data
-            int const nb_samples = hdr->format_nbs + 1;
-            int const nb_channels = hdr->format_nbc + 1;
-            size_t sample_size = VBanBitResolutionSize[static_cast<const VBanBitResolution>(hdr->format_bit &
-                                                                                            VBAN_BIT_RESOLUTION_MASK)];
-            size_t payload_size = nb_samples * sample_size * nb_channels;
-
-            // create buffers to push to players
-            std::vector<std::vector<float>> buffers;
-            int float_buffer_size = (payload_size / sample_size) / nb_channels;
-            for (int i = 0; i < nb_channels; i++) {
-                std::vector<float> new_buffer;
-                new_buffer.resize(float_buffer_size);
-                buffers.emplace_back(new_buffer);
-            }
-
-            // convert WAVE PCM multiplexed signal into floating point (SampleValue) buffers for each channel
-            for (size_t i = 0; i < float_buffer_size; i++)
-            {
-                for (int c = 0; c < nb_channels; c++)
-                {
-                    size_t pos = (i * nb_channels * 2) + (c * 2) + VBAN_HEADER_SIZE;
-                    char byte_1 = packet.data()[pos];
-                    char byte_2 = packet.data()[pos + 1];
-                    short original_value = ((static_cast<short>(byte_2)) << 8) | (0x00ff & byte_1);
-
-                    buffers[c][i] = ((float) original_value) / (float) 32768;
-                }
-            }
 
             // get sample rate
-            int const sample_rate_format   = hdr->format_SR & VBAN_SR_MASK;
+            int const sample_rate_format = hdr->format_SR & VBAN_SR_MASK;
             int sample_rate = 0;
-            if(utility::getSampleRateFromVBANSampleRateFormat(sample_rate, sample_rate_format, errorState))
-            {
+            if (utility::getSampleRateFromVBANSampleRateFormat(sample_rate, sample_rate_format, errorState)) {
                 // get stream name, use it to forward buffers to any registered stream audio receivers
                 std::string stream_name(hdr->streamname);
-                for(auto* receiver : mReceivers)
-                {
-                    if(receiver->getStreamName() == stream_name)
-                    {
+                for (auto *receiver: mReceivers) {
+                    if (receiver->getStreamName() == stream_name) {
+                        // get packet meta-data
+                        int const nb_samples = hdr->format_nbs + 1;
+                        int const nb_channels = hdr->format_nbc + 1;
+                        size_t sample_size = VBanBitResolutionSize[static_cast<const VBanBitResolution>(hdr->format_bit &
+                            VBAN_BIT_RESOLUTION_MASK)];
+                        size_t payload_size = nb_samples * sample_size * nb_channels;
+
+                        // create buffers to push to players
+                        std::vector<std::vector<float>> buffers;
+                        int float_buffer_size = (payload_size / sample_size) / nb_channels;
+                        for (int i = 0; i < nb_channels; i++) {
+                            std::vector<float> new_buffer;
+                            new_buffer.resize(float_buffer_size);
+                            buffers.emplace_back(new_buffer);
+                        }
+
+                        // convert WAVE PCM multiplexed signal into floating point (SampleValue) buffers for each channel
+                        for (size_t i = 0; i < float_buffer_size; i++)
+                        {
+                            for (int c = 0; c < nb_channels; c++)
+                            {
+                                size_t pos = (i * nb_channels * 2) + (c * 2) + VBAN_HEADER_SIZE;
+                                char byte_1 = packet.data()[pos];
+                                char byte_2 = packet.data()[pos + 1];
+                                short original_value = ((static_cast<short>(byte_2)) << 8) | (0x00ff & byte_1);
+
+                                buffers[c][i] = ((float) original_value) / (float) 32768;
+                            }
+                        }
+
                         receiver->pushBuffers(buffers);
                     }
                 }
@@ -85,33 +83,33 @@ namespace nap
 		}
         else
         {
-			nap::Logger::warn(*this, errorState.toString());
-		}
-	}
+            nap::Logger::warn(*this, errorState.toString());
+        }
+    }
 
 
 	bool VBANPacketReceiver::checkPacket(utility::ErrorState& errorState, nap::uint8 const* buffer, size_t size)
 	{
 		struct VBanHeader const* const hdr = (struct VBanHeader*)(buffer);
-		enum VBanProtocol protocol = static_cast<VBanProtocol>(VBAN_PROTOCOL_UNDEFINED_4);
-		enum VBanCodec codec = static_cast<VBanCodec>(VBAN_BIT_RESOLUTION_MAX);
+        enum VBanProtocol protocol = static_cast<VBanProtocol>(VBAN_PROTOCOL_UNDEFINED_4);
+        enum VBanCodec codec = static_cast<VBanCodec>(VBAN_BIT_RESOLUTION_MAX);
 
 		if(!errorState.check(buffer != 0, "buffer is null ptr"))
-			return false;
+            return false;
 
 		if(!errorState.check(size > VBAN_HEADER_SIZE, "packet too small"))
-			return false;
+            return false;
 
 		if(!errorState.check(hdr->vban == *(int32_t*)("VBAN"), "invalid vban magic fourc"))
-			return false;
+            return false;
 
 		if(!errorState.check(hdr->format_bit == VBAN_BITFMT_16_INT, "reserved format bit invalid value, only 16 bit PCM supported at this time"))
-			return false;
+            return false;
 
         if(!errorState.check((hdr->format_nbc + 1) > 0, "channel count cannot be 0 or smaller"))
             return false;
 
-		// check protocol and codec
+        // check protocol and codec
 		protocol        = static_cast<VBanProtocol>(hdr->format_SR & VBAN_PROTOCOL_MASK);
 		codec           = static_cast<VBanCodec>(hdr->format_bit & VBAN_CODEC_MASK);
 
@@ -119,46 +117,46 @@ namespace nap
 		{
 			switch (protocol)
 			{
-			case VBAN_PROTOCOL_SERIAL:
-			case VBAN_PROTOCOL_TXT:
-			case VBAN_PROTOCOL_UNDEFINED_1:
-			case VBAN_PROTOCOL_UNDEFINED_2:
-			case VBAN_PROTOCOL_UNDEFINED_3:
-			case VBAN_PROTOCOL_UNDEFINED_4:
-				errorState.fail("protocol not supported yet");
-			default:
-				errorState.fail("packet with unknown protocol");
-			}
+                case VBAN_PROTOCOL_SERIAL:
+                case VBAN_PROTOCOL_TXT:
+                case VBAN_PROTOCOL_UNDEFINED_1:
+                case VBAN_PROTOCOL_UNDEFINED_2:
+                case VBAN_PROTOCOL_UNDEFINED_3:
+                case VBAN_PROTOCOL_UNDEFINED_4:
+                    errorState.fail("protocol not supported yet");
+                default:
+                    errorState.fail("packet with unknown protocol");
+            }
 
-			return false;
+            return false;
 		}else
 		{
 			if(!errorState.check(codec == VBAN_CODEC_PCM, "unsupported codec"))
-				return false;
+                return false;
 
 			if(!checkPcmPacket(errorState, buffer, size))
-				return false;
-		}
+                return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
 
 	bool VBANPacketReceiver::checkPcmPacket(utility::ErrorState& errorState, const nap::uint8* buffer, size_t size)
 	{
-		// the packet is already a valid vban packet and buffer already checked before
+        // the packet is already a valid vban packet and buffer already checked before
 		struct VBanHeader const* const hdr = (struct VBanHeader*)(buffer);
 		enum VBanBitResolution const bit_resolution = static_cast<const VBanBitResolution>(hdr->format_bit & VBAN_BIT_RESOLUTION_MASK);
 		int const sample_rate_format   = hdr->format_SR & VBAN_SR_MASK;
 
 		if(!errorState.check(bit_resolution < VBAN_BIT_RESOLUTION_MAX, "invalid bit resolution"))
-			return false;
+            return false;
 
 		if(!errorState.check(sample_rate_format < VBAN_SR_MAXNUMBER, "invalid sample rate"))
-			return false;
+            return false;
 
-		return true;
-	}
+        return true;
+    }
 
 
 	void VBANPacketReceiver::registerStreamListener(IVBANStreamListener* receiver)
@@ -167,14 +165,14 @@ namespace nap
 		{
 			auto it = std::find_if(mReceivers.begin(), mReceivers.end(), [receiver](auto& a) { return a == receiver; });
 
-			assert(it == mReceivers.end()); // receiver already registered
+            assert(it == mReceivers.end()); // receiver already registered
 
 			if (it == mReceivers.end())
 			{
-				mReceivers.emplace_back(receiver);
-			}
-		});
-	}
+                mReceivers.emplace_back(receiver);
+            }
+        });
+    }
 
 
 	void VBANPacketReceiver::removeStreamListener(IVBANStreamListener* receiver)
@@ -183,16 +181,16 @@ namespace nap
 		{
 			auto it = std::find_if(mReceivers.begin(), mReceivers.end(), [receiver](auto& a)
 			{
-				return a == receiver;
-			});
+                return a == receiver;
+            });
 
-			assert(it != mReceivers.end()); // receiver not registered
+            assert(it != mReceivers.end()); // receiver not registered
 
 			if(it != mReceivers.end())
 			{
-			  	mReceivers.erase(it);
-			}
-		});
-	}
+                mReceivers.erase(it);
+            }
+        });
+    }
 
 }
